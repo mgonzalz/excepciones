7c17b2f7c1590e273a62fa16a13e494fd53dd468
BobiStudio <mgonzgarc@myuax.com>
2023-04-15 18:18:50 +0200

    except

--- ejercicio01.py
+++ ejercicio01.py
@@ -1,6 +1,11 @@
 '''
-Enunciado: escriba un programa que simule la conexión de un usuario a un sitio web para el que ya se ha registrado, solo con su dirección de correo electrónico (la gestión de una contraseña está fuera del alcance de esta sección). Este programa debe ofrecer la posibilidad al usuario de introducir una dirección de correo electrónico, y mostrará diferentes mensajes de error en función de la cadena introducida. El programa debe continuar si el correo electrónico indicado tiene un formato incorrecto y finalizar si no se reconoce el correo electrónico, ya que se podría tratar de un ciberataque. Importante: el método que analiza la cadena de caracteres no debe devolver ningún valor.
-
+Enunciado: escriba un programa que simule la conexión de un usuario a un sitio web para el que ya se ha
+registrado, solo con su dirección de correo electrónico (la gestión de una contraseña está fuera del
+alcance de esta sección). Este programa debe ofrecer la posibilidad al usuario de introducir una dirección
+de correo electrónico, y mostrará diferentes mensajes de error en función de la cadena introducida. El
+programa debe continuar si el correo electrónico indicado tiene un formato incorrecto y finalizar si no se
+reconoce el correo electrónico, ya que se podría tratar de un ciberataque. Importante: el método que
+analiza la cadena de caracteres no debe devolver ningún valor.
 '''
 
 import re # El método re permite trabajar con expresiones regulares
@@ -9,8 +14,8 @@ import csv
 #CÓDIGO
 class Correo_Electronico:
     def __init__(self, correo, name):
-        self.correo = correo
-        self.name = name
+        self.correo = str(correo)
+        self.name = str(name)
     def get_correo(self):
         return self.correo
     def get_name(self):
@@ -19,28 +24,33 @@ class Correo_Electronico:
         return self.name + " " + self.correo
 
 def get_from_csv(filename):
+    if not isinstance(filename, str):
+        raise TypeError("El archivo debe ser una cadena de caracteres")
     lista = []
-    with open(filename, newline='') as file:
-        reader = csv.reader(file)
-        for row in reader: # CONVERTIR EN CLASES
-            correos = Correo_Electronico(row[0], row[1])
-            lista.append(correos)
+    try:
+        with open(filename, newline='') as file:
+            reader = csv.reader(file)
+            for row in reader: # CONVERTIR EN CLASES
+                correos = Correo_Electronico(str(row[0]), str(row[1]))
+                lista.append(correos)
+    except SyntaxError:
+        print("Error de sintaxis")
     return lista
 
 def main():
     lista = get_from_csv("data/correos_disponibles.csv")
-    print("Bienvenido al sitio web")
+    print("Bienvenido al sitio web" + "\n" + "Introduzca su dirección de correo electrónico")
     try:
         correo = input("--> ")
-    except re.search(". * @. * \ .. *", correo):
-        print("Una dirección de correo electrónico debe tener el formato xxx@xxx.xx")
+        if not re.search(". * @. * \ .. *", correo):
+            raise ValueError
+    except ValueError:
+        print("El correo no tiene el formato correcto")
     else:
         for i in lista:
             if correo == i.get_correo():
                 print(f"Bienvenido {i.get_name()}")
                 break
-        else:
-            print("Cuenta bloqueada a causa de un ataque")
 
 
 if __name__ == '__main__':
